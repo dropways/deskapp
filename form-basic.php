784734f2b9be66afb841166cfee2e3a168b761af
krishaweb-PC\krishaweb <ankit.hingarajiya@gmail.com>
2018-03-06 00:22:41 +0530

    Basic form html

--- form-basic.php
+++ form-basic.php
@@ -47,7 +47,7 @@
 						</div>
 					</div>
 					<form>
-						<div class="form-group m-t-40 row">
+						<div class="form-group row">
 							<label class="col-sm-12 col-md-2 col-form-label">Text</label>
 							<div class="col-sm-12 col-md-10">
 								<input class="form-control" type="text" placeholder="Johnny Brown">
@@ -137,84 +137,83 @@
 							</div>
 						</div>
 					</form>
-					<div id="basic-form-scroll1">
-						<div class="collapse collapse-box" id="basic-form1" >
-							<div class="code-box">
-								<div class="clearfix">
-									<a href="javascript:;" class="btn btn-primary btn-sm code-copy pull-left"  data-clipboard-target="#copy-pre"><i class="fa fa-clipboard"></i> Copy Code</a>
-									<a href="#basic-form1" class="btn btn-primary btn-sm pull-right" rel="content-y"  data-toggle="collapse" role="button">Hide Code</a>
-								</div>
-								<pre><code class="xml copy-pre" id="copy-pre">
+					<div class="collapse collapse-box" id="basic-form1" >
+						<div class="code-box">
+							<div class="clearfix">
+								<a href="javascript:;" class="btn btn-primary btn-sm code-copy pull-left"  data-clipboard-target="#copy-pre"><i class="fa fa-clipboard"></i> Copy Code</a>
+								<a href="#basic-form1" class="btn btn-primary btn-sm pull-right" rel="content-y"  data-toggle="collapse" role="button">Hide Code</a>
+							</div>
+							<pre><code class="xml copy-pre" id="copy-pre">
 <form>
-	<div class="form-group m-t-40 row">
-		<label class="col-2 col-form-label">Text</label>
-		<div class="col-10">
+	<div class="form-group row">
+		<label class="col-sm-12 col-md-2 col-form-label">Text</label>
+		<div class="col-sm-12 col-md-10">
 			<input class="form-control" type="text" placeholder="Johnny Brown">
 		</div>
 	</div>
 	<div class="form-group row">
-		<label class="col-2 col-form-label">Search</label>
-		<div class="col-10">
+		<label class="col-sm-12 col-md-2 col-form-label">Search</label>
+		<div class="col-sm-12 col-md-10">
 			<input class="form-control" placeholder="Search Here" type="search">
 		</div>
 	</div>
 	<div class="form-group row">
-		<label class="col-2 col-form-label">Email</label>
-		<div class="col-10">
+		<label class="col-sm-12 col-md-2 col-form-label">Email</label>
+		<div class="col-sm-12 col-md-10">
 			<input class="form-control" value="bootstrap@example.com" type="email">
 		</div>
 	</div>
 	<div class="form-group row">
-		<label class="col-2 col-form-label">URL</label>
-		<div class="col-10">
+		<label class="col-sm-12 col-md-2 col-form-label">URL</label>
+		<div class="col-sm-12 col-md-10">
 			<input class="form-control" value="https://getbootstrap.com" type="url">
 		</div>
 	</div>
 	<div class="form-group row">
-		<label class="col-2 col-form-label">Telephone</label>
-		<div class="col-10">
+		<label class="col-sm-12 col-md-2 col-form-label">Telephone</label>
+		<div class="col-sm-12 col-md-10">
 			<input class="form-control" value="1-(111)-111-1111" type="tel">
 		</div>
 	</div>
 	<div class="form-group row">
-		<label class="col-2 col-form-label">Password</label>
-		<div class="col-10">
+		<label class="col-sm-12 col-md-2 col-form-label">Password</label>
+		<div class="col-sm-12 col-md-10">
 			<input class="form-control" value="password" type="password">
 		</div>
 	</div>
 	<div class="form-group row">
-		<label class="col-2 col-form-label">Number</label>
-		<div class="col-10">
+		<label class="col-sm-12 col-md-2 col-form-label">Number</label>
+		<div class="col-sm-12 col-md-10">
 			<input class="form-control" value="100" type="number">
 		</div>
 	</div>
 	<div class="form-group row">
-		<label for="example-datetime-local-input" class="col-2 col-form-label">Date and time</label>
-		<div class="col-10">
+		<label for="example-datetime-local-input" class="col-sm-12 col-md-2 col-form-label">Date and time</label>
+		<div class="col-sm-12 col-md-10">
 			<input class="form-control datetimepicker" placeholder="Choose Date anf time" type="text">
 		</div>
 	</div>
 	<div class="form-group row">
-		<label class="col-2 col-form-label">Date</label>
-		<div class="col-10">
+		<label class="col-sm-12 col-md-2 col-form-label">Date</label>
+		<div class="col-sm-12 col-md-10">
 			<input class="form-control date-picker" placeholder="Select Date" type="text">
 		</div>
 	</div>
 	<div class="form-group row">
-		<label class="col-2 col-form-label">Month</label>
-		<div class="col-10">
+		<label class="col-sm-12 col-md-2 col-form-label">Month</label>
+		<div class="col-sm-12 col-md-10">
 			<input class="form-control month-picker" placeholder="Select Month" type="text">
 		</div>
 	</div>
 	<div class="form-group row">
-		<label class="col-2 col-form-label">Time</label>
-		<div class="col-10">
+		<label class="col-sm-12 col-md-2 col-form-label">Time</label>
+		<div class="col-sm-12 col-md-10">
 			<input class="form-control time-picker" placeholder="Select time" type="text">
 		</div>
 	</div>
 	<div class="form-group row">
-		<label class="col-2 col-form-label">Select</label>
-		<div class="col-10">
+		<label class="col-sm-12 col-md-2 col-form-label">Select</label>
+		<div class="col-sm-12 col-md-10">
 			<select class="custom-select col-12">
 				<option selected="">Choose...</option>
 				<option value="1">One</option>
@@ -224,20 +223,239 @@
 		</div>
 	</div>
 	<div class="form-group row">
-		<label class="col-2 col-form-label">Color</label>
-		<div class="col-10">
+		<label class="col-sm-12 col-md-2 col-form-label">Color</label>
+		<div class="col-sm-12 col-md-10">
 			<input class="form-control" value="#563d7c" type="color">
 		</div>
 	</div>
 	<div class="form-group row">
-		<label class="col-2 col-form-label">Input Range</label>
-		<div class="col-10">
+		<label class="col-sm-12 col-md-2 col-form-label">Input Range</label>
+		<div class="col-sm-12 col-md-10">
 			<input class="form-control" value="50" type="range">
 		</div>
 	</div>
 </form>
-								</code></pre>
+							</code></pre>
+						</div>
+					</div>
+				</div>
+				<div class="pd-20 bg-white border-radius-4 box-shadow mb-30">
+					<div class="clearfix">
+						<div class="pull-left">
+							<h4 class="text-blue">horizontal Basic Forms</h4>
+							<p class="mb-30 font-14">All bootstrap element classies</p>
+						</div>
+						<div class="pull-right">
+							<a href="#horizontal-basic-form1" class="btn btn-primary btn-sm scroll-click" rel="content-y"  data-toggle="collapse" role="button">Source Code</a>
+						</div>
+					</div>
+					<form>
+						<div class="form-group">
+							<label>Text</label>
+							<input class="form-control" type="text" placeholder="Johnny Brown">
+						</div>
+						<div class="form-group">
+							<label>Email</label>
+							<input class="form-control" value="bootstrap@example.com" type="email">
+						</div>
+						<div class="form-group">
+							<label>URL</label>
+							<input class="form-control" value="https://getbootstrap.com" type="url">
+						</div>
+						<div class="form-group">
+							<label>Telephone</label>
+							<input class="form-control" value="1-(111)-111-1111" type="tel">
+						</div>
+						<div class="form-group">
+							<label>Password</label>
+							<input class="form-control" value="password" type="password">
+						</div>
+						<div class="form-group">
+							<label>Readonly input</label>
+							<input class="form-control" type="text" placeholder="Readonly input here…" readonly>
+						</div>
+						<div class="form-group">
+							<label>Disabled input</label>
+							<input type="text" class="form-control" placeholder="Disabled input" disabled="">
+						</div>
+						<div class="form-group">
+							<div class="row">
+								<div class="col-md-6 col-sm-12">
+									<label class="weight-600">Custom Checkbox</label>
+									<div class="custom-control custom-checkbox mb-5">
+										<input type="checkbox" class="custom-control-input" id="customCheck1">
+										<label class="custom-control-label" for="customCheck1">Check this custom checkbox</label>
+									</div>
+									<div class="custom-control custom-checkbox mb-5">
+										<input type="checkbox" class="custom-control-input" id="customCheck2">
+										<label class="custom-control-label" for="customCheck2">Check this custom checkbox</label>
+									</div>
+									<div class="custom-control custom-checkbox mb-5">
+										<input type="checkbox" class="custom-control-input" id="customCheck3">
+										<label class="custom-control-label" for="customCheck3">Check this custom checkbox</label>
+									</div>
+									<div class="custom-control custom-checkbox mb-5">
+										<input type="checkbox" class="custom-control-input" id="customCheck4">
+										<label class="custom-control-label" for="customCheck4">Check this custom checkbox</label>
+									</div>
+								</div>
+								<div class="col-md-6 col-sm-12">
+									<label class="weight-600">Custom Radio</label>
+									<div class="custom-control custom-radio mb-5">
+										<input type="radio" id="customRadio1" name="customRadio" class="custom-control-input">
+										<label class="custom-control-label" for="customRadio1">Toggle this custom radio</label>
+									</div>
+									<div class="custom-control custom-radio mb-5">
+										<input type="radio" id="customRadio2" name="customRadio" class="custom-control-input">
+										<label class="custom-control-label" for="customRadio2">Or toggle this other custom radio</label>
+									</div>
+									<div class="custom-control custom-radio mb-5">
+										<input type="radio" id="customRadio3" name="customRadio" class="custom-control-input">
+										<label class="custom-control-label" for="customRadio3">Or toggle this other custom radio</label>
+									</div>
+								</div>
 							</div>
+						</div>
+						<div class="form-group">
+							<label>Disabled select menu</label>
+							<select class="form-control" disabled="">
+								<option>Disabled select</option>
+							</select>
+						</div>
+						<div class="form-group">
+							<label>input plaintext</label>
+							<input type="text" readonly class="form-control-plaintext" value="email@example.com">
+						</div>
+						<div class="form-group">
+							<label>Textarea</label>
+							<textarea class="form-control"></textarea>
+						</div>
+						<div class="form-group">
+							<label>Help text</label>
+							<input type="text" class="form-control">
+							<small class="form-text text-muted">
+							  Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+							</small>
+						</div>
+						<div class="form-group">
+							<label>Example file input</label>
+							<input type="file" class="form-control-file form-control height-auto">
+						</div>
+						<div class="form-group">
+							<label>Custom file input</label>
+							<div class="custom-file">
+								<input type="file" class="custom-file-input">
+								<label class="custom-file-label">Choose file</label>
+							</div>
+						</div>
+					</form>
+					<div class="collapse collapse-box" id="horizontal-basic-form1" >
+						<div class="code-box">
+							<div class="clearfix">
+								<a href="javascript:;" class="btn btn-primary btn-sm code-copy pull-left"  data-clipboard-target="#horizontal-basic"><i class="fa fa-clipboard"></i> Copy Code</a>
+								<a href="#horizontal-basic-form1" class="btn btn-primary btn-sm pull-right" rel="content-y"  data-toggle="collapse" role="button">Hide Code</a>
+							</div>
+							<pre><code class="xml copy-pre" id="horizontal-basic">
+<form>
+	<div class="form-group">
+		<label>Text</label>
+		<input class="form-control" type="text" placeholder="Johnny Brown">
+	</div>
+	<div class="form-group">
+		<label>Email</label>
+		<input class="form-control" value="bootstrap@example.com" type="email">
+	</div>
+	<div class="form-group">
+		<label>URL</label>
+		<input class="form-control" value="https://getbootstrap.com" type="url">
+	</div>
+	<div class="form-group">
+		<label>Telephone</label>
+		<input class="form-control" value="1-(111)-111-1111" type="tel">
+	</div>
+	<div class="form-group">
+		<label>Password</label>
+		<input class="form-control" value="password" type="password">
+	</div>
+	<div class="form-group">
+		<label>Readonly input</label>
+		<input class="form-control" type="text" placeholder="Readonly input here…" readonly>
+	</div>
+	<div class="form-group">
+		<label>Disabled input</label>
+		<input type="text" class="form-control" placeholder="Disabled input" disabled="">
+	</div>
+	<div class="form-group">
+		<div class="row">
+			<div class="col-md-6 col-sm-12">
+				<label class="weight-600">Custom Checkbox</label>
+				<div class="custom-control custom-checkbox mb-5">
+					<input type="checkbox" class="custom-control-input" id="customCheck1">
+					<label class="custom-control-label" for="customCheck1">Check this custom checkbox</label>
+				</div>
+				<div class="custom-control custom-checkbox mb-5">
+					<input type="checkbox" class="custom-control-input" id="customCheck2">
+					<label class="custom-control-label" for="customCheck2">Check this custom checkbox</label>
+				</div>
+				<div class="custom-control custom-checkbox mb-5">
+					<input type="checkbox" class="custom-control-input" id="customCheck3">
+					<label class="custom-control-label" for="customCheck3">Check this custom checkbox</label>
+				</div>
+				<div class="custom-control custom-checkbox mb-5">
+					<input type="checkbox" class="custom-control-input" id="customCheck4">
+					<label class="custom-control-label" for="customCheck4">Check this custom checkbox</label>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-12">
+				<label class="weight-600">Custom Radio</label>
+				<div class="custom-control custom-radio mb-5">
+					<input type="radio" id="customRadio1" name="customRadio" class="custom-control-input">
+					<label class="custom-control-label" for="customRadio1">Toggle this custom radio</label>
+				</div>
+				<div class="custom-control custom-radio mb-5">
+					<input type="radio" id="customRadio2" name="customRadio" class="custom-control-input">
+					<label class="custom-control-label" for="customRadio2">Or toggle this other custom radio</label>
+				</div>
+				<div class="custom-control custom-radio mb-5">
+					<input type="radio" id="customRadio3" name="customRadio" class="custom-control-input">
+					<label class="custom-control-label" for="customRadio3">Or toggle this other custom radio</label>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="form-group">
+		<label>Disabled select menu</label>
+		<select class="form-control" disabled="">
+			<option>Disabled select</option>
+		</select>
+	</div>
+	<div class="form-group">
+		<label>input plaintext</label>
+		<input type="text" readonly class="form-control-plaintext" value="email@example.com">
+	</div>
+	<div class="form-group">
+		<label>Textarea</label>
+		<textarea class="form-control"></textarea>
+	</div>
+	<div class="form-group">
+		<label>Help text</label>
+		<input type="text" class="form-control">
+		<small class="form-text text-muted">
+		  Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+		</small>
+	</div>
+	<div class="form-group">
+		<label>Example file input</label>
+		<input type="file" class="form-control-file form-control height-auto">
+	</div>
+	<div class="form-group">
+		<label>Custom file input</label>
+		<div class="custom-file">
+			<input type="file" class="custom-file-input">
+			<label class="custom-file-label">Choose file</label>
+		</div>
+	</div>
+</form></code></pre>
 						</div>
 					</div>
 				</div>
