e56c0b57b0a75927408d5c0cd933734f4cdcf225
Ravi Mehta <rvmehta745@gmail.com>
2018-04-17 16:27:51 +0530

    js modification

--- vendors/scripts/script.js
+++ vendors/scripts/script.js
@@ -32185,38 +32185,14 @@ jQuery(document).ready(function(){
 	$("[data-border]").each(function() {
 		$(this).css('border', $(this).attr('data-border'));
 	});
+
+	$("#accordion-menu").vmenuModule({
+		Speed: 400,
+		autostart: false,
+		autohide: true
+	});
+
 });
-
-function CopyToClipboard(value, showNotification, notificationText) {
-	var $temp = $("<input>");
-	if(value != ''){
-		var $temp = $("<input>");
-		$("body").append($temp);
-		$temp.val(value).select();
-		document.execCommand("copy");
-		$temp.remove();
-	}
-	if (typeof showNotification === 'undefined') {
-		showNotification = true;
-	}
-	if (typeof notificationText === 'undefined') {
-		notificationText = "Copied to clipboard";
-	}
-	var notificationTag = $("div.copy-notification");
-	if (showNotification && notificationTag.length == 0) {
-		notificationTag = $("<div/>", { "class": "copy-notification", text: notificationText });
-		$("body").append(notificationTag);
-
-		notificationTag.fadeIn("slow", function () {
-			setTimeout(function () {
-				notificationTag.fadeOut("slow", function () {
-					notificationTag.remove();
-				});
-			}, 1000);
-		});
-	}
-}
-
 
 // sidebar menu accordion
 (function($) {
@@ -32278,10 +32254,33 @@ function CopyToClipboard(value, showNotification, notificationText) {
 
 	}
 })(window.jQuery || window.Zepto);
-$(document).ready(function() {
-	$("#accordion-menu").vmenuModule({
-		Speed: 400,
-		autostart: false,
-		autohide: true
-	});
-});
+
+function CopyToClipboard(value, showNotification, notificationText) {
+	var $temp = $("<input>");
+	if(value != ''){
+		var $temp = $("<input>");
+		$("body").append($temp);
+		$temp.val(value).select();
+		document.execCommand("copy");
+		$temp.remove();
+	}
+	if (typeof showNotification === 'undefined') {
+		showNotification = true;
+	}
+	if (typeof notificationText === 'undefined') {
+		notificationText = "Copied to clipboard";
+	}
+	var notificationTag = $("div.copy-notification");
+	if (showNotification && notificationTag.length == 0) {
+		notificationTag = $("<div/>", { "class": "copy-notification", text: notificationText });
+		$("body").append(notificationTag);
+
+		notificationTag.fadeIn("slow", function () {
+			setTimeout(function () {
+				notificationTag.fadeOut("slow", function () {
+					notificationTag.remove();
+				});
+			}, 1000);
+		});
+	}
+}
