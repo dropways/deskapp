409020f1ae6c6ce486c88aa871296fd344fb30ff
DESKTOP-RUDG6I0\ANKIT <ankit@krishaweb.net>
2018-04-17 15:57:41 +0530

    footer modification

--- include/footer.php
+++ include/footer.php
@@ -1,3 +1,3 @@
 	<div class="footer-wrap bg-white pd-20 mb-20 border-radius-5 box-shadow">
-		DeskApp - Bootstrap 4 Admin Template By Ankit Hingarajiya
+		DeskApp - Bootstrap 4 Admin Template By <a href="https://github.com/dropways" target="_blank">DROPWAYS</a>
 	</div>
