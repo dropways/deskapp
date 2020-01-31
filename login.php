ec1eeb8a96d482157d3d7e34b0762b04b6ada2d0
ankithingarajiya <ankit@krishaweb.net>
2020-01-31 18:25:31 +0530

    login page ui change

--- login.php
+++ login.php
@@ -3,39 +3,79 @@
 <head>
 	<?php include('include/head.php'); ?>
 </head>
-<body>
-	<div class="login-wrap customscroll d-flex align-items-center flex-wrap justify-content-center pd-20">
-		<div class="login-box bg-white box-shadow pd-30 border-radius-5">
-			<img src="vendors/images/login-img.png" alt="login" class="login-img">
-			<h2 class="text-center mb-30">Login</h2>
-			<form>
-				<div class="input-group custom input-group-lg">
-					<input type="text" class="form-control" placeholder="Username">
-					<div class="input-group-append custom">
-						<span class="input-group-text"><i class="fa fa-user" aria-hidden="true"></i></span>
-					</div>
+<body class="login-page">
+	<div class="login-header box-shadow">
+		<div class="container-fluid d-flex justify-content-between align-items-center">
+			<div class="brand-logo">
+				<a href="login.php">
+					<img src="vendors/images/deskapp-logo.svg" alt="">
+				</a>
+			</div>
+			<div class="login-menu">
+				<ul>
+					<li><a href="#">Register</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="login-wrap d-flex align-items-center flex-wrap justify-content-center">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-md-7">
+					<img src="vendors/images/login-page-img.png" alt="">
 				</div>
-				<div class="input-group custom input-group-lg">
-					<input type="password" class="form-control" placeholder="**********">
-					<div class="input-group-append custom">
-						<span class="input-group-text"><i class="fa fa-lock" aria-hidden="true"></i></span>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-sm-6">
-						<div class="input-group">
-							<!--
-								use code for form submit
-								<input class="btn btn-outline-primary btn-lg btn-block" type="submit" value="Sign In">
-							-->
-							<a class="btn btn-outline-primary btn-lg btn-block" href="index.php">Sign In</a>
+				<div class="col-md-5">
+					<div class="login-box bg-white box-shadow border-radius-10">
+						<div class="login-title">
+							<h2 class="text-center font-weight-700 text-primary">Login Here</h2>
 						</div>
-					</div>
-					<div class="col-sm-6">
-						<div class="forgot-password padding-top-10"><a href="forgot-password.php">Forgot Password</a></div>
-					</div>
+						<form>
+							<div class="input-group custom">
+								<input type="text" class="form-control" placeholder="Username">
+								<div class="input-group-append custom">
+									<span class="input-group-text"><i class="fa fa-user" aria-hidden="true"></i></span>
+								</div>
+							</div>
+							<div class="input-group custom">
+								<input type="password" class="form-control" placeholder="**********">
+								<div class="input-group-append custom">
+									<span class="input-group-text"><i class="fa fa-lock" aria-hidden="true"></i></span>
+								</div>
+							</div>
+							<div class="row pb-30">
+								<div class="col-6">
+									<div class="custom-control custom-checkbox">
+										<input type="checkbox" class="custom-control-input" id="customCheck1">
+										<label class="custom-control-label" for="customCheck1">Remember</label>
+									</div>
+								</div>
+								<div class="col-6">
+									<div class="forgot-password"><a href="forgot-password.php">Forgot Password</a></div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-12">
+									<div class="input-group mb-0">
+										<!--
+											use code for form submit
+											<input class="btn btn-outline-primary btn-lg btn-block" type="submit" value="Sign In">
+										-->
+										<a class="btn btn-outline-primary btn-lg btn-block" href="index.php">Sign In</a>
+									</div>
+									<div class="font-16 weight-600 pt-10 pb-10 text-center" data-color="#707373">OR</div>
+									<div class="input-group mb-0">
+										<!--
+											use code for form submit
+											<input class="btn btn-outline-primary btn-lg btn-block" type="submit" value="Sign In">
+										-->
+										<a class="btn btn-primary btn-lg btn-block" href="index.php">Register To Create Account</a>
+									</div>
+								</div>
+							</div>
+						</form>
+					</div>				
 				</div>
-			</form>
+			</div>
 		</div>
 	</div>
 	<?php include('include/script.php'); ?>
